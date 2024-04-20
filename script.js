4811ff739928af2ce61c95ddc5d393c1a76cf324
sugoiTakeshi <marcus.silva@eaportal.org>
2024-04-20 23:15:03 +0000

    javascript do index

---
+++ script.js
@@ -0,0 +1,11 @@
+let botoes = document.querySelectorAll(".botoes button")
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", guardarNota)
+})
+
+function guardarNota(evento) {
+    let nota = evento.target.innerText
+
+    localStorage.setItem("nota", nota)
+}
